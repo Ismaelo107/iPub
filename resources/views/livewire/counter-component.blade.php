1a14e9617ffec0599f82447102e6dbeb48e125bd
ismaelo107 <ismaeloeg@iescastelar.com>
2025-02-11 20:36:58 +0100

    Probando Livewire

---
+++ resources/views/livewire/counter-component.blade.php
@@ -0,0 +1,6 @@
+<div>
+    <h1>Hola</h1>
+    <h3>El contador es : {{$counter}}</h3>
+    <button class="bg-blue-500" wire:click="increment">+</button>
+    <button wire:click="decrement">-</button>
+</div>
